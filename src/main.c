6d836a68cfc46f6ce324bcd1fb118645202c3a0b
hoverdd <lenyamenshov@icloud.com>
2025-09-01 00:39:15 +0300

    feat(main): Add main entry point and glue code to parse arguments and process files

--- src/main.c
+++ src/main.c
@@ -4,31 +4,12 @@
 #include "io.h"
 #include "cli.h"
 
-int main(int argc, char *argv[]) {
-    char **files;
-    int files_num;
+int main(int argc, const char *argv[]) {
+    Args args;
+    parse_args(argc, argv, &args); 
 
-    int result = parse_args(argc, argv, &files, &files_num);
-    if (result != 0) {
-        return 1;
-    }
+    process_files(&args);
 
-    for (int i = 0; i < files_num; i++) {
-        if (file_exists(files[i])) {
-            if (is_dir(files[i])) {
-                fprintf(stderr, "'%s': Is a directory\n", files[i]);
-                continue;
-            }
-            io_errors_t err = read_and_print_file(files[i]);
-            if (err != SUCCESS) {
-                fprintf(stderr, "%s\n", io_error_to_string(err));
-            }
-        } else {
-            fprintf(stderr, "File: '%s' does not exist\n", files[i]);
-        }
-    }
-
-    cleanup_files(files);
-
+    cleanup_files(args.files);
     return 0;
 }
