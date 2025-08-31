0b6ed563604711b12594363aaf516f1e8a6d4334
hoverdd <lenyamenshov@icloud.com>
2025-09-01 00:41:02 +0300

    feat(io): Add file I/O functions: read_and_print_file, print_file_numbered, print_file_with_separator, file existence checks

--- include/io.h
+++ include/io.h
@@ -11,9 +11,21 @@ typedef enum {
 
 io_errors_t read_and_print_file(const char *filename);
 
+io_errors_t print_file_numbered(const char *filename);
+
+io_errors_t print_file_with_separator(const char *filename, const char *sep);
+
+void trim_new_line(char *s);
+
 int file_exists(const char *filename);
 
 int is_dir(const char *filename);
+
+void io_print_file(const char *filename);
+
+void io_print_file_numbered(const char *filename);
+
+void io_print_file_with_separator(const char *filename, const char *sep);
 
 char *io_error_to_string(io_errors_t error);
 
--- src/io.c
+++ src/io.c
@@ -2,6 +2,7 @@
 #include "config.h"
 
 #include <stdio.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
@@ -33,6 +34,63 @@ io_errors_t read_and_print_file(const char *filename) {
     return SUCCESS;
 }
 
+io_errors_t print_file_numbered(const char *filename) {
+    char buffer[BUF_SIZE];
+
+    FILE *file = fopen(filename, "rb");
+    if (!file) return ERR_OPENNING_FILE;
+
+    int line = 1;
+    while (fgets(buffer, sizeof(buffer), file) != NULL) {
+        fprintf(stdout, "%6d  %s", line, buffer);
+        line++;
+    }
+    
+    if (ferror(file)) {
+        fclose(file);
+        return ERR_READ; // read error
+    }
+
+    int err = fclose(file);
+    if (err != 0) {
+        return ERR_CLOSE;
+    }
+    return SUCCESS;
+}
+
+io_errors_t print_file_with_separator(const char *filename, const char *sep) {
+    FILE *file = fopen(filename, "rb");
+    if (!file) return ERR_OPENNING_FILE;
+
+    char buffer[BUF_SIZE];
+
+    while (fgets(buffer, sizeof(buffer), file) != NULL) {
+        trim_new_line(buffer);
+        fprintf(stdout, "%s", buffer);
+        fprintf(stdout, "%s\n", sep);
+    }
+
+    fprintf(stdout, "\n");
+
+    if (ferror(file)) {
+        fclose(file);
+        return ERR_READ; // read error
+    }
+
+    int err = fclose(file);
+    if (err != 0) {
+        return ERR_CLOSE;
+    }
+    return SUCCESS;
+}
+
+void trim_new_line(char *s) {
+    ssize_t len = strlen(s);
+    if (len > 0 && s[len - 1] == '\n') {
+        s[len - 1] = '\0';
+    }
+}
+
 int file_exists(const char *filename) {
     int is_exist = 0;
 
@@ -49,6 +107,27 @@ int is_dir(const char *path) {
     stat(path, &path_stat);
 
     return S_ISDIR(path_stat.st_mode);
+}
+
+void io_print_file(const char *filename) {
+    io_errors_t err = read_and_print_file(filename);
+    if (err != SUCCESS) {
+        fprintf(stderr, "%s\n", io_error_to_string(err));
+    }
+}
+
+void io_print_file_numbered(const char *filename) {
+    io_errors_t err = print_file_numbered(filename);
+    if (err != SUCCESS) {
+        fprintf(stderr, "%s\n", io_error_to_string(err));
+    }
+}
+
+void io_print_file_with_separator(const char *filename, const char *sep) {
+    io_errors_t err = print_file_with_separator(filename, sep);
+    if (err != SUCCESS) {
+        fprintf(stderr, "%s\n", io_error_to_string(err));
+    }
 }
 
 char *io_error_to_string(io_errors_t error) {
